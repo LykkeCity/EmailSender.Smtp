f03271e7d7b0eccb666d4eca15db3ccec6ff33da
amigin <amigin@gmail.com>
2017-02-04 03:08:12 +0200

    Add project files.

---
+++ src/Lykke.EmailSender.Smtp/EmailSenderToSmtp.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace Lykke.EmailSender.Smtp
+{
+
+    public class EmailSenderToSmtpSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public string LocalDomain { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public string DisplayName { get; set; }
+        public string From { get; set; }
+
+    }
+
+    public class EmailSenderToSmtp : IEmailSender
+    {
+        private readonly EmailSenderToSmtpSettings _settings;
+
+        public EmailSenderToSmtp(EmailSenderToSmtpSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public async Task SendEmailAsync(EmailModel model)
+        {
+
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_settings.DisplayName, _settings.From));
+            emailMessage.To.Add(new MailboxAddress(string.Empty, model.Email));
+            emailMessage.Subject = model.Subject;
+
+            var messageBody = new TextPart(model.IsHtml ? TextFormat.Html : TextFormat.Plain) { Text = model.Body };
+
+            if (model.Attachments != null)
+                if (model.Attachments.Length > 0)
+                {
+                    var multipart = new Multipart("mixed") {messageBody};
+
+                    foreach (var attachment in model.Attachments)
+                    {
+
+
+                        var attachmentData = new MemoryStream(attachment.Data);
+                        var attach = new MimePart(attachment.Mime)
+                        {
+                            ContentObject = new ContentObject(attachmentData),
+                            FileName = attachment.FileName
+                        };
+
+                        multipart.Add(attach);
+                    }
+                    emailMessage.Body = multipart;
+                }
+
+                else
+                {
+                    emailMessage.Body = messageBody;
+                }
+
+
+            await SendEmailMessage(emailMessage);
+
+        }
+
+        private async Task SendEmailMessage(MimeMessage emailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                client.LocalDomain = _settings.LocalDomain;
+                await client.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.None);
+                await client.AuthenticateAsync(_settings.Login, _settings.Password);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
